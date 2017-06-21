158714e666ab1d89a0fc8555460f19ce57ed49bb
Ney Marques <neyrpg@gmail.com>
2017-06-20 23:12:43 -0300

    Projeto Android - Estudo com maps

---
+++ app/src/main/java/br/com/giltech/android_mapas_basico/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.giltech.android_mapas_basico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
